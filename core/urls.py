297547c19ea8824f3178245b3b76acb810ab29ac
kdecamilo <c.silva2@duocuc.cl>
2022-06-09 13:40:36 -0400

    integracion django 2

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import home
+from .views import home, quienes_somos
+
 
 urlpatterns = [
     path('',home,name="home"),
+    path ('quienes-somos/', quienes_somos, name='quienes_somos'),
 ]
--- core/views.py
+++ core/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'core/home.html')
+    
+def quienes_somos(request):
+    return render(request, 'core/quienes_somos.html')
+
