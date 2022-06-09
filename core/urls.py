563200e29fde7b3cacc7735f71e7e83418517841
Pedro <ped.sanmartin@duocuc.cl>
2022-06-09 13:38:37 -0400

    productos casi

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import home
+from .views import home, productos
 
 urlpatterns = [
-    path('',home,name="home"),
+    path('', home, name="home"),
+    path('productos/', productos, name="productos"),
 ]
+
+
