de5e59099bbc228882d4abdfbedcb8213ef184d0
LaDChoromota <lui.salamanca@duocuc.cl>
2022-06-09 23:36:39 -0400

    home css completo guantes completo api con fallos
    :/

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path
-from .views import home
 
+from django.urls import path
+from .views import home, api, guantes
 urlpatterns = [
     path('',home,name="home"),
+    path('api/',api,name="api"),
+    path('guantes',guantes,name="guantes")
 ]
