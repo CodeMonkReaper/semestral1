5b22cb0f45794a866b04244b402a87c77c605bfe
Pedro <ped.sanmartin@duocuc.cl>
2022-06-10 10:41:09 -0400

    toy chato

--- core/urls.py
+++ core/urls.py
@@ -1,21 +1,11 @@
-<<<<<<< HEAD
 from django.urls import path
-from .views import home, productos, macetas, quienes_somos
-=======
->>>>>>> Luis
-
-from django.urls import path
-from .views import home, api, guantes
+from .views import home, productos, macetas, quienes_somos, guantes
 urlpatterns = [
-<<<<<<< HEAD
+
     path('', home, name="home"),
     path('productos/', productos, name="productos"),
     path('quienes-somos/', quienes_somos, name='quienes_somos'),
-    path("macetas/", macetas, name="macetas")
-    ]
-=======
-    path('',home,name="home"),
-    path('api/',api,name="api"),
+    path("macetas/", macetas, name="macetas"), 
     path('guantes',guantes,name="guantes")
-]
->>>>>>> Luis
+    ]
+
--- core/views.py
+++ core/views.py
@@ -3,7 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'core/home.html')
-<<<<<<< HEAD
 
 def productos(request):
     return render(request, 'core/productos.html')
@@ -13,9 +12,9 @@ def quienes_somos(request):
 
 def macetas(request):
     return render(request, 'core/macetas.html')
-=======
+
 def api(request):
     return render(request,'core/api.html')
+    
 def guantes(request):
     return render(request,'core/guantes.html')
->>>>>>> Luis
