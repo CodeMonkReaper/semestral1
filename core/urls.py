0dad4ff5ff732db5d8becade9571d01fa2798cd2
kdecamilo <c.silva2@duocuc.cl>
2022-06-10 10:29:03 -0400

    semillas, tierras, tijeras (hay que implementar productos) y register con fallos

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,13 @@
+from atexit import register
 from django.urls import path
-from .views import home, quienes_somos
+from .views import home, quienes_somos, register, semillas, tierras, tijeras
 
 
 urlpatterns = [
     path('',home,name="home"),
-    path ('quienes-somos/', quienes_somos, name='quienes_somos'),
-]
+    path('quienes-somos/', quienes_somos, name='quienes_somos'),
+    path('register/', register, name='register'),
+    path('semillas/', semillas, name='semillas'),
+    path('tierras/', tierras, name='tierras'),
+    path('tijeras/', tijeras, name='tijeras')
+]   
--- core/views.py
+++ core/views.py
@@ -7,3 +7,14 @@ def home(request):
 def quienes_somos(request):
     return render(request, 'core/quienes_somos.html')
 
+def register(request):
+    return render(request, 'core/register.html'),
+
+def semillas(request):
+    return render(request, 'core/semillas.html'),
+
+def tierras(request):
+    return render(request, 'core/tierras.html'),
+
+def tijeras(request):
+    return render(request, 'core/tijeras.html')
