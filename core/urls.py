74902fd10ba266d014ca9172d5fd64ac63801609
LaDChoromota <lui.salamanca@duocuc.cl>
2022-06-12 16:35:32 -0400

    nose

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,13 @@
+from unicodedata import name
 from django.urls import path
-from .views import home, productos, macetas, quienes_somos, guantes
+from .views import home, productos, macetas, quienes_somos, guantes,api
 urlpatterns = [
 
     path('', home, name="home"),
     path('productos/', productos, name="productos"),
     path('quienes-somos/', quienes_somos, name="quienes_somos"),
     path('macetas/', macetas, name="macetas"), 
-    path('guantes/',guantes,name="guantes")
+    path('guantes/',guantes,name="guantes"),
+    path('api/',api,name="api"),
     ]
 
