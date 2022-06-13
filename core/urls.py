ee8396b3aea012727c3d01b07a41208abdcb80ea
kdecamilo <c.silva2@duocuc.cl>
2022-06-12 21:43:41 -0400

    urlpatterns

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,10 @@
-from .views import home, productos, macetas, guantes, semillas, tierras, tijeras
+from .views import home, productos, macetas, guantes, semillas, tierras, tijeras, quienes_somos
 from django.urls import path
-[
+urlpatterns = [
     path('', home, name="home"),
     path('productos/', productos, name="productos"),
     path('macetas/', macetas, name="macetas"), 
+    path('quienes-somos/',quienes_somos, name="quienes_somos" ),
     path('guantes/',guantes,name="guantes"),
     path('tierras/',tierras,name="tierras"),
     path('semillas/',semillas,name="semillas"),
