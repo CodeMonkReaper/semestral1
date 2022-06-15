96df09b2767aa1dc14a4f32dd5a129c2c8b399d6
Pedro <ped.sanmartin@duocuc.cl>
2022-06-15 15:53:56 -0400

    quité productos

--- core/views.py
+++ core/views.py
@@ -4,38 +4,11 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'core/home.html')
 
-def productos(request):
-    return render(request, 'core/productos.html')
-
 def quienes_somos(request):
     return render(request, 'core/quienes_somos.html')
 
-def macetas(request):
-    return render(request, 'core/macetas.html')
-
 def api(request):
     return render(request,'core/api.html')
-    
-def guantes(request):
-    return render(request,'core/guantes.html')
-
-def tierras(request):
-    return render(request,'core/tierras.html')
-
-def tijeras(request):
-    return render(request,'core/tijeras.html')
-
-def semillas(request):
-    return render(request,'core/semillas.html')
-
-def portamangueras(request):
-    return render(request,'core/portamangueras.html')
-
-def mangueras(request):
-    return render(request,'core/mangueras.html')
-
-def palas(request):
-    return render(request,'core/palas.html')
 
 def register(request):
     return render(request,'core/register.html')
@@ -43,5 +16,11 @@ def register(request):
 def login(request):
     return render(request,'core/login.html')
 
-def ofertas(request):
-    return render(request,'core/ofertas.html')
+def productos(request):
+    return render(request,'core/productos.html')
+
+def comprar(request):
+    return render(request,'core/comprar.html')
+
+def creditodebito(request):
+    return render(request,'core/creditodebito.html')
