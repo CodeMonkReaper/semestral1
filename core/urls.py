056cb2fc336e0b69fdffc43645ea145d5b5599bb
AguanteMessi <ped.sanmartin@duocuc.cl>
2022-06-15 23:42:25 -0400

    xd

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,13 @@
-from .views import api, home, productos, quienes_somos,  register, login
+from .views import home, quienes_somos, api, register, login, productos, comprar, creditodebito, agregar_prod
 from django.urls import path
 urlpatterns = [
     path('', home, name="home"),
-    path('productos/', productos, name="productos"), 
-    path('api/',api,name="api"),
-    path('api/',api,name="api"),
+    path('quienes-somos/',quienes_somos, name="quienes_somos" ),
+    path('register/',register,name="register"),
+    path('login/',login,name="login"),
+    path('productos/', productos,name="productos"),
+    path('api/',api, name="api"),
+    path('comprar/',comprar, name="comprar"),
+    path('creditodebito/',creditodebito, name="creditodebito"),
+    path('agregarprod/',agregar_prod, name="agregarprod"),
     ]
-
