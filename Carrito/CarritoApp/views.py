cf53c1f624e743b4e38df83504092896baf58548
kdecamilo <c.silva2@duocuc.cl>
2022-06-16 23:34:02 -0400

    modificando modulo

--- Carrito/CarritoApp/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render, HttpResponse
-
-# Create your views here.
-
-def tienda(request):
-    #return render(request,'core/tienda.html') 
-    #return HttpResponse ("Hola Pythonizando")
-    return render(request,'tienda.html') 
-
--- core/urls.py
+++ core/urls.py
@@ -1,14 +1,12 @@
-from .views import home, quienes_somos, api, register, login, productos, comprar, creditodebito, agregar_prod
+from .views import home, quienes_somos, api, register, login, comprar, creditodebito
 from django.urls import path
 urlpatterns = [
     path('', home, name="home"),
     path('quienes-somos/',quienes_somos, name="quienes_somos" ),
     path('register/',register,name="register"),
     path('login/',login,name="login"),
-    path('productos/', productos,name="productos"),
     path('api/',api, name="api"),
     path('comprar/',comprar, name="comprar"),
     path('creditodebito/',creditodebito, name="creditodebito"),
-    path('agregarprod/',agregar_prod, name="agregarprod"),
     
     ]
